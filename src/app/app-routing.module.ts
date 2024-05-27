a9c53980326b4c4c571188ce8a0dba4cc2bde370
Mac10 <alumno@MacBook-Pro-10.local>
2024-05-27 11:53:32 -0500

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { Pagina1Component } from './components/pagina1/pagina1.component';
 import { Pagina2Component } from './components/pagina2/pagina2.component';
+import { ContactenosComponent } from './components/contactenos/contactenos.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'pagina1', component: Pagina1Component },
-  { path: 'pagina2', component: Pagina2Component }
+  { path: 'pagina2', component: Pagina2Component },
+  { path: 'contactenos', component: ContactenosComponent },
+  { path: 'marcas', component: MarcasComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'producto', component: ProductoComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HomeComponent } from './components/home/home.component';
 import { Pagina1Component } from './components/pagina1/pagina1.component';
 import { Pagina2Component } from './components/pagina2/pagina2.component';
 import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
+import { ContactenosComponent } from './components/contactenos/contactenos.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { NavmenuComponent } from './components/navmenu/navmenu.component';
     HomeComponent,
     Pagina1Component,
     Pagina2Component,
-    NavmenuComponent
+    NavmenuComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    ProductoComponent,
+    MarcasComponent,
+    ContactenosComponent
   ],
   imports: [
     BrowserModule,
